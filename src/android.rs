29fc592addca0beffb968a29c70420e323a00a27
Vivek <itsvks19@gmail.com>
2025-07-18 13:59:09 +0530

    add toast duration option to Android interface

--- src/android.rs
+++ src/android.rs
@@ -1,10 +1,19 @@
-#[link(wasm_import_module = "Android")]
-unsafe extern "C" {
-    fn show_toast_impl(ptr: *const u8, len: usize);
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ToastDuration {
+    Short = 0,
+    Long = 1,
 }
 
-pub fn show_toast(msg: &str) {
+#[link(wasm_import_module = "Android")]
+unsafe extern "C" {
+    fn show_toast_impl(ptr: *const u8, len: usize, duration: u8);
+}
+
+pub fn show_toast(msg: &str, duration: Option<ToastDuration>) {
+    let duration = duration.unwrap_or(ToastDuration::Short);
+
     unsafe {
-        show_toast_impl(msg.as_ptr(), msg.len());
+        show_toast_impl(msg.as_ptr(), msg.len(), duration as u8);
     }
 }
